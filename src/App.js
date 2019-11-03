b2967391e8fc14185171d6182c48863de753c39a
jonjj2016 <43015018+jonjj2016@users.noreply.github.com>
2019-11-03 16:36:50 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    input: ''
+  };
+
+  render() {
+    //console.log(this.state.input);
+    return (
+      <div>
+        <h1>recipy</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
